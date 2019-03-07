0abaff02bd82082d1f4eda0eceb4197a60272bf6
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-03-07 11:09:08 +0100

    Fixed a problem with main.

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
     llvm::Module *module;                       // module to process
     std::string modulePath;                     // full path for the file to process
 
-    modulePath = std::string(args_info.ll_path) + std::string(args_info.ll_filename);
+    modulePath = std::string(args_info.ll_path) + "/"+ std::string(args_info.ll_filename);
 
     // check if the .ll file of the module exists and can be opened
     std::fstream file;
@@ -60,7 +60,7 @@ int main(int argc, char **argv) {
 
     char *opt_argv[] = {progName,
                         debug};
-    llvm::cl::ParseCommandLineOptions(2, opt_argv, "");
+    //llvm::cl::ParseCommandLineOptions(2, opt_argv, "");
 
     // Run on module
     {
